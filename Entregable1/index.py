14b395125816b3344a6a10d9a86af9e6c478fe92
rulas99 <raul.990527@gmail.com>
2024-03-06 08:53:35 -0600

    feat(download): pdf download function

--- Entregable1/index.py
+++ Entregable1/index.py
@@ -1,13 +1,14 @@
 import argparse
 import src.bulk.bulk_requestor as bulk_requestor
 import src.bulk.connections_requestor as connections_requestor
+from src.utils.download_pdf import download_pdf
 from time import sleep
 
 def get_args():
     '''
     Get program arguments specified by the user
     '''
-    opt_choices = ['request_bulk', 'request_connections']
+    opt_choices = ['request_bulk', 'request_connections','download_pdf']
     
     parser = argparse.ArgumentParser()
     requiredNamedArgs = parser.add_argument_group('required named arguments')
@@ -40,6 +41,10 @@ def main():
             connections_requestor.request_data(chunk_size=6)
             print("Sleeping for 10 minutes")
             sleep(600)
+            
+    if (option == 'download_pdf'):
+        download_pdf()
+
 
 if __name__ == "__main__":
     main()
---
+++ Entregable1/src/utils/download_pdf.py
@@ -0,0 +1,33 @@
+from glob import glob
+from pandas import read_parquet
+from requests import get as get_request
+from tqdm import tqdm
+
+def download_pdf():
+    """
+    Función principal que gestiona la descarga de archivos PDF que aún no están presentes en la carpeta local.
+    Lee un catálogo de archivos PDF desde un archivo Parquet y compara con los archivos ya descargados.
+    """
+    files = [f.split('/')[-1][:-4] for f in glob('./data/pdf/*.pdf')]
+
+    catalogue = read_parquet('./data/api_request_results/pdf_catalogue.parquet')
+    catalogue = catalogue[~catalogue['pdf_url'].isin(files)]
+
+    urls_name = catalogue[['pdf_url', 'paperId']].values.tolist()
+    
+    for url, name in tqdm(urls_name):
+        try:
+            req = get_request(url)
+            if req.status_code == 200:
+                with open('./data/pdf/' + name + '.pdf', 'wb') as f:
+                    f.write(req.content)
+            else:
+                raise Exception('Error')
+        except Exception:
+            print(f'Error downloading {url}')
+            pass
+
+    
+    
+
+
